2e68777e724cd70b5122d8cdee667f2e3bd65135
Neruthes <i@neruthes.xyz>
2020-12-10 21:34:40 +0000

    Regular update 2020-12-10T20:00:15-00:00

--- cronjobs/NDPS1/root/1hr1.sh
+++ cronjobs/NDPS1/root/1hr1.sh
@@ -1,11 +1,13 @@
 source /root/DEV/NDevShellRC/index.linux.sh
 
-cd /home/neruthes/DEV/NDevCron
+cd /root/DEV/NDevCron
 git pull origin master -f
 
-cd /home/neruthes/DEV/NDevShellRC
-git pull origin master -f
+if [[ -e /home/.mounted ]]; then
+    cd /home/neruthes/DEV/NDevShellRC
+    git pull origin master -f
 
-cd /home/neruthes/DEV/NDevMgr
-git pull origin master -f
-cat /home/neruthes/DEV/NDevMgr/ssh/* > /root/.ssh/authorized_keys
+    cd /home/neruthes/DEV/NDevMgr
+    git pull origin master -f
+    cat /home/neruthes/DEV/NDevMgr/ssh/* > /root/.ssh/authorized_keys
+fi
