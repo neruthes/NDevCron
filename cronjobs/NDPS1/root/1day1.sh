2dcc5e449fea7a6b531e0f47c3ff0e6b09ff0d9b
Neruthes <i@neruthes.xyz>
2020-12-17 14:14:41 +0000

    Regular update Thu Dec 17 14:10:28 UTC 2020

--- cronjobs/NDPS1/root/1day1.sh
+++ cronjobs/NDPS1/root/1day1.sh
@@ -1,3 +1,7 @@
 if [[ -e /mnt/NEPd2/Data/.mounted ]]; then
     /root/DEV/NDevShellRC/bin/saturation-git-keeper pull-repos
 fi
+
+systemctl restart avahi-daemon
+systemctl restart tinc@NTincNet
+systemctl restart netatalk
