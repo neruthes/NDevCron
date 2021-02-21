8855b9dd58d3d80b32072c92d920a3d50c725cd5
Neruthes <i@neruthes.xyz>
2021-02-21 19:22:45 +0000

    Regular update Sun Feb 21 18:53:04 UTC 2021

--- cronjobs/NDLT6/neruthes/1hr2.sh
+++ cronjobs/NDLT6/neruthes/1hr2.sh
@@ -1,5 +1,3 @@
-bash /Users/Neruthes/Developer/NDevShellRC/bin/NDevSync-landdns > /dev/null 2>&1
-
 IPADDR="$(ip addr | grep 192.168.31.26)"
 if [[ "${IPADDR:0:13}" == "192.168.31.26" ]]; then
     bash /Users/Neruthes/Developer/NDevShellRC/bin/NDevSync-wanddns > /dev/null 2>&1
